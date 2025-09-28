40afe7ae681aa47249bcf137abf9b643a30bcdd6
Ahmed Bahaa <ab3854093@gamil.com>
2025-09-29 02:01:33 +0300

    add SQLITE AND HELPER

---
+++ lib/core/helper.dart
@@ -0,0 +1,82 @@
+import 'dart:async';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:sqflite/sqflite.dart';
+import '../models/order_model.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+
+  factory DatabaseHelper() => _instance;
+
+  DatabaseHelper._internal();
+
+  static Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    final directory = await getApplicationDocumentsDirectory();
+    final path = join(directory.path, "orders.db");
+
+    return await openDatabase(path, version: 1, onCreate: _onCreate);
+  }
+
+  Future _onCreate(Database db, int version) async {
+    await db.execute('''
+      CREATE TABLE orders (
+        billSrl TEXT PRIMARY KEY,
+        status TEXT,
+        totalPrice REAL,
+        date TEXT
+      )
+    ''');
+  }
+
+  //CRUD
+
+  // Insert Order
+  Future<int> insertOrder(OrderModel order) async {
+    final db = await database;
+    return await db.insert(
+      "orders",
+      order.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  // Get All Orders
+  Future<List<OrderModel>> getOrders() async {
+    final db = await database;
+    final maps = await db.query("orders");
+
+    return List.generate(maps.length, (i) {
+      return OrderModel.fromMap(maps[i]);
+    });
+  }
+
+  // Delete Order
+  Future<int> deleteOrder(String billSrl) async {
+    final db = await database;
+    return await db.delete(
+      "orders",
+      where: "billSrl = ?",
+      whereArgs: [billSrl],
+    );
+  }
+
+  // Update Order
+  Future<int> updateOrder(OrderModel order) async {
+    final db = await database;
+    return await db.update(
+      "orders",
+      order.toMap(),
+      where: "billSrl = ?",
+      whereArgs: [order.billSrl],
+    );
+  }
+}
--- lib/models/order_model.dart
+++ lib/models/order_model.dart
@@ -11,6 +11,7 @@ class OrderModel {
     required this.date,
   });
 
+
   factory OrderModel.fromJson(Map<String, dynamic> json) {
     return OrderModel(
       billSrl: json['BILL_SRL'].toString(),
@@ -18,5 +19,27 @@ class OrderModel {
       totalPrice: double.tryParse(json['BILL_AMT'].toString()) ?? 0.0,
       date: json['BILL_DATE'].toString(),
     );
+  }
+
+
+  factory OrderModel.fromMap(Map<String, dynamic> map) {
+    return OrderModel(
+      billSrl: map['billSrl'].toString(),
+      status: map['status'].toString(),
+      totalPrice: map['totalPrice'] is int
+          ? (map['totalPrice'] as int).toDouble()
+          : map['totalPrice'] as double,
+      date: map['date'].toString(),
+    );
+  }
+
+
+  Map<String, dynamic> toMap() {
+    return {
+      'billSrl': billSrl,
+      'status': status,
+      'totalPrice': totalPrice,
+      'date': date,
+    };
   }
 }
